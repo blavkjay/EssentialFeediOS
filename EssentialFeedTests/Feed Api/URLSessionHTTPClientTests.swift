5ba32e46508c8c2549d1e920cd376da00a3e0559
Olajuwon Balogun <olajuwon@softcom.ng>
2020-12-18 22:25:17 +0100

    Delivers data and response on successful HTTPURL request

--- EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -24,9 +24,11 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { _,_,error in
+        session.dataTask(with: url) { data,response,error in
             if let error = error {
                 completion(.failure(error))
+            }else if let data = data, data.count > 0, let response = response as? HTTPURLResponse{
+                completion(.success(data, response))
             }else {
                 completion(.failure(UnexpectedValuesRepresentation()))
             }
@@ -86,6 +88,26 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertNotNil(resultErrorFor(data: anyData(), response: nonHTTPURLResponse(), error: nil))
     }
     
+    func test_getFromURL_succeedsOnHTTPURLResponseWithData() {
+        let data = anyData()
+        let response = anyHTTPURLResponse()
+        URLProtocolStub.stub(data: data, response: response, error: nil)
+        let exp = expectation(description: "Wait for description")
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case let .success(receivedData, receivedResponse):
+                XCTAssertEqual(receivedData, data)
+                XCTAssertEqual(receivedResponse.url, response.url)
+                XCTAssertEqual(receivedResponse.statusCode, response.statusCode)
+            default:
+                XCTFail("Expected success, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     //MARK:- Helper
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
@@ -131,6 +153,8 @@ class URLSessionHTTPClientTests: XCTestCase {
         wait(for: [exp], timeout: 1)
         return receivedError
     }
+    
+    
     
     private class URLProtocolStub: URLProtocol {
         
