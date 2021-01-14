9b992a8319d7e2026d49ba1d7439ba00ced8fa69
Olajuwon Balogun <olajuwon@softcom.ng>
2021-01-14 09:03:18 +0100

    correct wrong data in expectedItem function, Use Ephemeral URL Session configuration to avoid sharing state across test execution ( in-disk cache artifacts)

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -10,6 +10,7 @@ import XCTest
 import EssentialFeed
 
 class EssentialFeedAPIEndToEndTests: XCTestCase {
+    
 
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
         
@@ -31,7 +32,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
         let testServerUrl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerUrl, client: client)
         trackMemoryLeaks(client,file: file, line: line)
         trackMemoryLeaks(loader,file: file, line: line)
@@ -53,39 +54,40 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
     
     private func id(at index: Int) -> UUID {
+        print("index \(index)")
         return UUID(uuidString: [
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8"
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
         ][index])!
     }
     
     private func description(at index: Int) -> String? {
-        return [ "description 1",
-                 "description 2",
+        return [ "Description 1",
                  nil,
-                 "description 4",
-                 "description 5",
+                 "Description 3",
                  nil,
-                 "description 7",
-                 "description 8"
+                 "Description 5",
+                 "Description 6",
+                 "Description 7",
+                 "Description 8"
         ][index]
     }
     
     private func location(at index: Int) -> String? {
-        return [ "location 1",
-                 "location 2",
+        return [ "Location 1",
+                 "Location 2",
                  nil,
-                 "location 4",
-                 "location 5",
                  nil,
-                 "location 7",
-                 "location 8"
+                 "Location 5",
+                 "Location 6",
+                 "Location 7",
+                 "Location 8"
         ][index]
     }
     
